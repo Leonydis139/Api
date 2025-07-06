7b0c4a5d2e3676a6f475cd2c4c74c6ebe2d19c15
Leonydis139 <juangreyling42@gmail.com>
2025-07-06 02:12:08 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import QuantumRequestClient from './QuantumRequestClient';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QuantumRequestClient />
+  </React.StrictMode>
+);
