1a41764a09a215f1b42d0266888399aca9120f50
Leonydis139 <juangreyling42@gmail.com>
2025-07-06 04:06:05 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import QuantumRequestClient from './QuantumRequestClient';
+import { sendQuantumRequest } from './QuantumRequestClient';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QuantumRequestClient />
-  </React.StrictMode>
-);
+const App = () => {
+  const handleClick = async () => {
+    try {
+      const result = await sendQuantumRequest();
+      alert(JSON.stringify(result, null, 2));
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>QuantumRequest Client</h1>
+      <button onClick={handleClick}>Send Quantum Request</button>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
